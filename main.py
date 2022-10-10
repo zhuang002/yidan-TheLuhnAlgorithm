f05a3a8052cbf2ccdbed5022b133319918c871da
zhuang002 <zhuang001@gmail.com>
2022-10-09 22:07:25 -0400

    refactor some variable names

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ def add_digits(num:int):
     return (num % 10) + int(num / 10)
 
 
-def get_partial_sum(number:str):
-    doubleDigit = True
-    sum = 0
+def get_partial_sum(number:str)->int:
+    double_digit = True
+    partial_sum:int = 0
     for index in range(len(number)-1,-1,-1):
         c = number[index]
-        if doubleDigit:
-            sum += add_digits(int(c)*2)
+        if double_digit:
+            partial_sum += add_digits(int(c)*2)
         else:
-            sum += int(c)
-        doubleDigit = not doubleDigit
-    return sum
+            partial_sum += int(c)
+        double_digit = not double_digit
+    return partial_sum
 
 
 def get_last_digit(number):
@@ -23,12 +23,14 @@ def get_last_digit(number):
     else:
         return 10 - remainder
 
+
 def do_test_case():
     numbers = input().split(' ')
     for num in numbers:
         digit = get_last_digit(num)
         print(digit, end='')
     print()
+
 
 for i in range(5):
     do_test_case()
